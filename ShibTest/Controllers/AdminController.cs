22f6d18799ec40c288d78f39b5869849c5ae22da
Scott Kirkland <srkirkland@gmail.com>
2011-08-09 14:41:05 -0700

    created an admin controller which will use attributes for security based off of the forms auth ticket

---
+++ ShibTest/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShibTest.Controllers
+{
+    [Authorize] //Need to be logged in for this
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}

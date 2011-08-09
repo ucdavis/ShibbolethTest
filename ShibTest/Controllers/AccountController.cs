6a9049623b355b0fb8f170f4c2c4688892e16df3
Scott Kirkland <srkirkland@gmail.com>
2011-08-09 14:25:38 -0700

    Added an account controller which logs on via the server variables

---
+++ ShibTest/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System.Security;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ShibTest.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public void Logon()
+        {
+            var user = Request.ServerVariables["HTTP_REMOTEUSER"];
+
+            if (string.IsNullOrWhiteSpace(user))
+            {
+                throw new SecurityException("You need to be logged in with Shibboleth!");
+            }
+
+            FormsAuthentication.RedirectFromLoginPage(user, false);
+        }
+
+    }
+}

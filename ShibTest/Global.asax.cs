70bc7536830e4f9c398dc395ad3d0eb72b10756f
Scott Kirkland <srkirkland@gmail.com>
2012-10-17 09:44:39 -0700

    ignore *.sso so shib requests can go through

--- ShibTest/Global.asax.cs
+++ ShibTest/Global.asax.cs
@@ -20,6 +20,7 @@ namespace ShibTest
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.IgnoreRoute("{resource}.sso/{*pathInfo}");
 
             routes.MapRoute(
                 "Default", // Route name
